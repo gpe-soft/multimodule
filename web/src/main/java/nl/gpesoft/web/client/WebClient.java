75a9260b869e29f0dd4713e1472e682f2b5d2759
peterse73 <peterse73@gmail.com>
2017-04-01 21:22:18 +0200

    Add webclient

--- web/src/main/java/nl/gpesoft/web/client/WebClient.java
+++ web/src/main/java/nl/gpesoft/web/client/WebClient.java
@@ -1,10 +1,34 @@
 package nl.gpesoft.web.client;
 
-import nl.gpesoft.domain.Employee;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 public class WebClient implements IEmployee {
 
-    public Employee getEmployee(Long id) {
-        return new Employee();
+    public static void main(String[] args) {
+        WebClient webClient = new WebClient();
+        webClient.isEmployeePresent(1L);
+    }
+
+   public Boolean isEmployeePresent(Long id) {
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target("http://localhost:8080/multimodule/rest");
+        WebTarget resourceWebTarget = webTarget.path("employee/" + id);
+
+        Invocation.Builder invocationBuilder = resourceWebTarget.request(MediaType.TEXT_PLAIN_TYPE);
+        invocationBuilder.header("some-header", "true");
+
+        Response response = invocationBuilder.get();
+        if (response.getStatus() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+        }
+        else {
+            System.out.println(response.readEntity(Boolean.class));
+            return (Boolean) response.getEntity();
+        }
     }
 }
